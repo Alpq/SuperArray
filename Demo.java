917b8e13d97b85f3b2d1758399ec8c028d90882c
Alpq <doymazalp@gmail.com>
2020-11-10 08:23:20 -0500

    Fixed removeDuplicates which had a for loop with a i value that started too small. findOverlap() fixed so that the list started from the values at a. Fixed copy/paste error in zip() so that it added values from both arrays.

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ public class Demo{
 
   public static void removeDuplicates(SuperArray s)
   {
-    for (int i = s.get(); i > 0 ; i -- ) {
+    for (int i = s.get() -1; i > 0 ; i -- ) {
       if (s.indexOf(s.get(i)) != i){
         s.remove(i);
       }
@@ -14,12 +14,10 @@ public class Demo{
     SuperArray wow = new SuperArray(Math.max(a.get() + 1, b.get() + 1));
     removeDuplicates(a);
     removeDuplicates(b);
-    SuperArray smallest = (a.get() > b.get()) ? b : a;
-    SuperArray largest = (a.get() > b.get()) ? a : b;
-    for (int i = 0;i < smallest.get() ;i ++ ) {
-      if (largest.indexOf(smallest.get(i)) != 1)
+    for (int i = 0; i < a.get() ;i ++ ) {
+      if (b.indexOf(a.get(i)) != -1)
       {
-        wow.add(smallest.get(i));
+        wow.add(a.get(i));
       }
     }
     return wow;
@@ -31,19 +29,20 @@ public class Demo{
     for (int i = 0;i < bigger; i ++)
     {
       if(a.get() >= i) {wowo.add(a.get(i));}
-      if(b.get() >= i) {wowo.add(a.get(i));}
+      if(b.get() >= i) {wowo.add(b.get(i));}
     }
     return wowo;
   }
+    public static void main(String[]args){
+    /*  SuperArray words = new SuperArray();
+      //grouped to save vertical space
+      words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+      words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+      words.add("una");    words.add("ebi");     words.add("toro");
 
-  public static void main(String[]args){
-    SuperArray words = new SuperArray();
-    //grouped to save vertical space
-    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
-    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    words.add("una");    words.add("ebi");     words.add("toro");
-
-    System.out.println(words);
-    removeDuplicates(words);
-    System.out.println(words);  }
+      System.out.println(words);
+      removeDuplicates(words);
+      System.out.println(words);  }
+    */
+  }
 }
