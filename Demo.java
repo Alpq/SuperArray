02bef75a286a17c3ea2377f010ccbc476096a860
Alpq <doymazalp@gmail.com>
2020-11-10 08:25:34 -0500

    Added credit for test cases and removed the main functoin out of a comment.

--- Demo.java
+++ Demo.java
@@ -1,3 +1,4 @@
+// Thanks to Steven Lei for helping with test cases
 public class Demo{
 
 
@@ -34,15 +35,13 @@ public class Demo{
     return wowo;
   }
     public static void main(String[]args){
-    /*  SuperArray words = new SuperArray();
-      //grouped to save vertical space
+     SuperArray words = new SuperArray();
       words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
       words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
       words.add("una");    words.add("ebi");     words.add("toro");
 
       System.out.println(words);
       removeDuplicates(words);
-      System.out.println(words);  }
-    */
+      System.out.println(words);
   }
 }
